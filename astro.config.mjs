4726d931d235ef404fc15232a0b1a333d53a1d13
Patricio Frascaroli <pmfrascaroli@gmail.com>
2024-02-19 17:39:13 -0300

    astro.config update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import vercelAdapter from "@astrojs/vercel-adapter";
+import vercelAdapter from "@sveltejs/adapter-vercel";
 import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
