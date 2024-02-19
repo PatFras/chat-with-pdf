52316d2529890ad2cc79e752bacf5ab7dbe14937
Patricio Frascaroli <pmfrascaroli@gmail.com>
2024-02-19 14:47:33 -0300

    astro & svelte configuration update

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), svelte()],
+  integrations: [tailwind(), svelte({ preprocess: [] })],
   output: "server",
 });
